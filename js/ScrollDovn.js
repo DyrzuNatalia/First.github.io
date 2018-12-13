1db388753877c1409ac056defa83608350018d12
DyrzuNatali <vfdhjdbxyy123>
2018-12-14 01:10:32 +0200

    *

--- js/ScrollDovn.js
+++ js/ScrollDovn.js
@@ -8,11 +8,11 @@
     var windowCoords = document.documentElement.clientHeight;
     (function scroll() {
       if (window.pageYOffset < windowCoords) {
-        window.scrollBy(0, 10);
+        window.scrollBy(0, 200);
         setTimeout(scroll, 0);
       }
       if (window.pageYOffset > windowCoords) {
-        window.scrollTo(0, windowCoords);
+       
       }
     })();
   }
